22e37c79ef03d3a7d707544a0b37bdb4ec0e51fd
Nitin K.M <nitinkm320@gmail.com>
2025-10-16 18:45:03 +0530

    exp10

---
+++ exp10/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char expr[20], ops[10], arg1s[10], arg2s[10];
+    char operators[] = "/*+-";
+    int i, j, count = 0;
+
+    printf("Enter expression: ");
+    scanf("%s", expr);
+
+    for (j = 0; j < 4; j++) {
+        for (i = 0; expr[i]; i++) {
+            if (expr[i] == operators[j]) {
+                ops[count] = expr[i];
+                arg1s[count] = expr[i - 1];
+                arg2s[count] = expr[i + 1];
+                expr[i - 1] = count + '0'; 
+                strcpy(&expr[i], &expr[i + 2]); 
+                count++;
+                i = -1; 
+            }
+        }
+    }
+
+    printf("\n--- Three-Address Code ---\n");
+    for (i = 0; i < count; i++) {
+        printf("t%d = ", i + 1);
+        isdigit(arg1s[i]) ? printf("t%d ", arg1s[i] - '0' + 1) : printf("%c ", arg1s[i]);
+        printf("%c ", ops[i]);
+        isdigit(arg2s[i]) ? printf("t%d\n", arg2s[i] - '0' + 1) : printf("%c\n", arg2s[i]);
+    }
+
+    printf("\n--- Quadruples ---\nOp\tArg1\tArg2\tResult\n");
+    for (i = 0; i < count; i++) {
+        printf("%c\t", ops[i]);
+        isdigit(arg1s[i]) ? printf("t%d\t", arg1s[i] - '0' + 1) : printf("%c\t", arg1s[i]);
+        isdigit(arg2s[i]) ? printf("t%d\t", arg2s[i] - '0' + 1) : printf("%c\t", arg2s[i]);
+        printf("t%d\n", i + 1);
+    }
+
+    printf("\n--- Triples ---\nPos\tOp\tArg1\tArg2\n");
+    for (i = 0; i < count; i++) {
+        printf("(%d)\t%c\t", i, ops[i]);
+        isdigit(arg1s[i]) ? printf("(%c)\t", arg1s[i]) : printf("%c\t", arg1s[i]); 
+        isdigit(arg2s[i]) ? printf("(%c)\n", arg2s[i]) : printf("%c\n", arg2s[i]); 
+    }
+
+    return 0;
+}
--- exp9/main.c
+++ exp9/main.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
-    char expr[50], op;
-    int i, j, temp_count = 1;
+    char expr[20], ops[10], arg1s[10], arg2s[10];
     char operators[] = "/*+-";
+    int i, j, count = 0;
 
-    printf("Enter the expression: ");
+    printf("Enter expression: ");
     scanf("%s", expr);
-    printf("\n--- Three-Address Code ---\n");
 
-    for (j = 0; j < strlen(operators); j++) {
-        op = operators[j];
-        for (i = 0; expr[i] != '\0'; i++) {
-            if (expr[i] == op) {
-                printf("t%d = %c %c %c\n", temp_count, expr[i - 1], op, expr[i + 1]);
-                expr[i - 1] = temp_count + '0';
-                
-                for (int k = i; expr[k] != '\0'; k++) {
-                    expr[k] = expr[k + 2];
-                }
-                temp_count++;
-                i = -1;
+    for (j = 0; j < 4; j++) {
+        for (i = 0; expr[i]; i++) {
+            if (expr[i] == operators[j]) {
+                ops[count] = expr[i];
+                arg1s[count] = expr[i - 1];
+                arg2s[count] = expr[i + 1];
+                expr[i - 1] = count + '0'; 
+                strcpy(&expr[i], &expr[i + 2]); 
+                count++;
+                i = -1; // Rescan
             }
         }
     }
-    
-    printf("result = t%d\n", temp_count-1);
-    return 0;
+
+    // Print all results
+    printf("\n--- Three-Address Code ---\n");
+    for (i = 0; i < count; i++) {
+        printf("t%d = ", i + 1);
+        isdigit(arg1s[i]) ? printf("t%d ", arg1s[i] - '0' + 1) : printf("%c ", arg1s[i]); 
+        printf("%c ", ops[i]);
+        isdigit(arg2s[i]) ? printf("t%d\n", arg2s[i] - '0' + 1) : printf("%c\n", arg2s[i]); 
+    }
 }
